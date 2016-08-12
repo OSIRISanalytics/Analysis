03a2fe1bd6bc2820cde0416213eb4904fa57a381
Tom Bishop <tomwbish@gmail.com>
2016-08-13 09:30:39 +1000

    arb test

--- arb_test1.R
+++ arb_test1.R
@@ -56,19 +56,25 @@ for (i in seq_along(betfair_games[,1])) {
 # Adding in the Osiris ID to the data sets
 # Only keep games where we know we have a definite match
 betfair_short = betfair %>%
-  left_join(betfair_games, by = "market_id") %>% select(-Team_1,-Team_2,-Team_3) %>%
+  left_join(betfair_games, by = "market_id") %>% select(-price_selection_id,-Team_1,-Team_2,-Team_3) %>%
   filter(!is.na(osiris_id)) 
 
 betfair_short = betfair_short %>%
   mutate(team = rep(c("Team_1","Team_2","Team_3"), nrow(betfair_short) / 3))%>%
-  gather(market, value, available_to_back_price,available_to_back_market,available_to_lay_price,available_to_lay_market) %>%
-  spread(team, runner) %>%
+  gather(market, value, runner_name,available_to_back_price,available_to_back_market,available_to_lay_price,available_to_lay_market) %>%
   unite(surrogate,team, market) %>%
-  spread(surrogate, value)
+  spread(surrogate, value) %>%
+  select(log_time,osiris_id, Team_1_runner_name,Team_2_runner_name,Team_3_runner_name,Team_1_available_to_back_price,Team_2_available_to_back_price,Team_3_available_to_back_price)
 
 pinnacle_short = pinnacle %>%
   left_join(pin_games, by = "event_id", copy=FALSE) %>%
-  filter(!is.na(osiris_id))
+  filter(!is.na(osiris_id), market_type == 'moneyline') %>%
+  select(log_time, osiris_id, home_team.x, away_team.x, market_value, market_odds) %>%
+  mutate(runner_name = ifelse(market_value==1,home_team.x, ifelse(market_value==0,"The Draw",away_team.x)),
+         team = ifelse(market_value==1,"Team_1", ifelse(market_value==0,"Team_3","Team_2"))) %>%
+  select(-market_value,-home_team.x,-away_team.x) %>% gather(var,value,runner_name,market_odds) %>%
+  unite(surrogate, team, var) %>%
+  spread(surrogate, value)
 
 
 # Now we join
@@ -77,18 +83,34 @@ joined = sqldf::sqldf(
 "
 SELECT
   a.log_time,
-  b.league_name,
-  a.gom
+  a.osiris_id,
+  a.Team_1_runner_name as home_team,
+  a.Team_2_runner_name as away_team,
+  a.Team_1_available_to_back_price as betfair_home,
+  a.Team_2_available_to_back_price as betfair_away,
+  a.Team_3_available_to_back_price as betfair_draw,
+  b.Team_1_market_odds as pin_home,
+  b.Team_2_market_odds as pin_away,
+  b.Team_3_market_odds as pin_draw
 FROM
   betfair_short a
   INNER JOIN pinnacle_short b ON (
     a.osiris_id = b.osiris_id AND
-    a.log_time = b.log_time)
-                      
-                      
-                      ")
+    a.log_time = b.log_time)")
 
+#Apply commision
+joined$betfair_home = (1+(as.numeric(joined$betfair_home)-1)*.95)
+joined$betfair_away = (1+(as.numeric(joined$betfair_away)-1)*.95)
+joined$betfair_draw = (1+(as.numeric(joined$betfair_draw)-1)*.95)
 
+joined$pin_home = as.numeric(joined$pin_home)
+joined$pin_away = as.numeric(joined$pin_away)
+joined$pin_draw = as.numeric(joined$pin_draw)
 
+joined_fil = subset(joined, pin_home >=1)
 
+joined_fil$hmax = ifelse(joined_fil$betfair_home > joined_fil$pin_home, joined_fil$betfair_home,joined_fil$pin_home)
+joined_fil$amax =  ifelse(joined_fil$betfair_away > joined_fil$pin_away, joined_fil$betfair_away,joined_fil$pin_away)
+joined_fil$dmax =  ifelse(joined_fil$betfair_draw > joined_fil$pin_draw, joined_fil$betfair_draw,joined_fil$pin_draw)
 
+joined_fil$arb = 1 / joined_fil$hmax + 1/joined_fil$amax+ 1/joined_fil$dmax
